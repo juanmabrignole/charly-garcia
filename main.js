a2158da06407fb5a7bc3b94eef1a23098362778c
Juanma Brignole <juanmabrignole@gmail.com>
2023-07-31 11:25:10 -0300

    Clase 01 JS

---
+++ main.js
@@ -0,0 +1,46 @@
+//comentarios de una sola línea
+/*comentarios de una línea o más*/
+
+//Tipos de variables var let const
+//Declaración e inicialización de variables
+
+//Declaro la variable
+//Con var puedo crear o instanciar variables con el mismo nombre cuantas veces quiera
+var nombreUsuario;
+
+//Inicializo la variable
+nombreUsuario = "Juanma Brignole";
+nombreUsuario = 38;
+
+nombreUsuario = "Martín Herman";
+
+////Con let puedo crear o instanciar variables con el mismo nombre sólo una vez. Tampoco puedo crearla sin asignarle algún
+
+let numeroUsuario;
+numerousuario= 320;
+let numeroUsuario2;
+numeroUsuario2 = 340;
+
+////Con const asignar un valor a una variable sólo una vez
+
+const numeroc = 4;
+// numeroc = 5 //Error, ya no se puede reasignar el valor
+
+//Operador de concatenacion +
+let nombre = "Juanma";
+let apellido = 'Brignole';
+const blanco = " ";
+
+let nombreCompleto = nombre + blanco + apellido;
+console.log(nombre);
+
+
+
+
+
+
+
+
+
+
+
